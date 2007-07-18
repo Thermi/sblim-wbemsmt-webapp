53e389623ab4b58cfe68a7e0cba183ac6431f563
mbauschert <mbauschert>
2007-07-18 16:51:28 +0000

    Fix [ 1756335 ] wbemsmt-webapp: synchronize concurrent ajax requests

--- webapp/scripts/ajaxPanelController.js
+++ webapp/scripts/ajaxPanelController.js
@@ -1,55 +1,94 @@
-//var debugWindow = window.open("debug.html", "Zweitfenster", "width=300,height=400,left=100,top=200,resizable=yes");
-function addText(text)
-{
-	//alert(text)
-	//if (debugWindow.document.forms.debugform)
-	{
-		//debugWindow.document.forms.debugform.debugOut.value = debugWindow.document.forms.debugform.debugOut.value + "\n" + text;
-	}
-}
-
+if (typeof console == 'undefined') {
+// Try to be compatible with other browsers
+// Only use firebug logging when available
+console = new Object;
+console.trace = function() {};
+console.log = function() {};
+console.debug = function() {};
+console.info = function() {};
+console.warn = function() {};
+console.error = function() {};
+console.time = function() {};
+console.timeEnd = function() {};
+console.count = function() {};
+console.profile = function() {};
+console.profileEnd = function() {};
+console.group = function() {};
+console.groupEnd = function() {};
+console.dir = function() {};
+console.dirxml = function() {};
+} 
 
 dojo.provide("org.sblim.wbemsmt.ajax");
 
 dojo.require("dojo.dom.*");
 
+//flag to synchronize the requests
+var ajaxRequestIsRunning = false;
+
 org.sblim.wbemsmt.ajax.AjaxPanelController = function(formId)
 {
     this.form = dojo.byId(formId);
     this.windowTimeout = new Array();
+    this.threads = new Array();
     
     if(typeof this.form == "undefined" || this.form.tagName.toLowerCase() != "form")
     {
         alert("AjaxPanelController: Form with id:" + formId + " not found!");
         return;
     }
+    
+    this.execute();
 }
 
 // starting/stopping point of automatically partial page refresh
 
 org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.startPeriodicalUpdate = function(refreshTimeout, zoneId)
 {
-	addText("startPeriodicalUpdate");
+	console.log("startPeriodicalUpdate");
 	if (refreshTimeout > 0)
 	{
-	    var content = new Array;
-	    content["org.sblim.wbemsmt.ajax.AjaxPanelController.triggeredComponents"] = zoneId;
-	    this.doAjaxSubmit(content, refreshTimeout, zoneId);
+		var idx = this.threads.length;
+		this.threads[idx] = new Object();
+		this.threads[idx]["refreshTimeout"] = refreshTimeout;
+		this.threads[idx]["zoneId"] = zoneId;
 	}
 };
 
 org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.stopPeriodicalUpdate = function(zoneId)
 {
-	addText("stopPeriodicalUpdate");
+	console.log("stopPeriodicalUpdate");
 	
 	if (this.windowTimeout[zoneId])
 	{
 
-    	addText("Remove Timeout: " + zoneId);
+    	console.log("Remove Timeout: " + zoneId);
 	    window.clearTimeout(this.windowTimeout[zoneId]);
 	    delete this.windowTimeout[zoneId];
 	}
 };
+
+// Execute the first ajax submit of the current threads list
+// waits a few milliseconds and execute itself again
+org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.execute = function()
+{
+	if (this.threads.length > 0)
+	{
+		var zoneId = this.threads[0]["zoneId"];
+		var refreshTimeout = this.threads[0]["refreshTimeout"];
+		var content = new Array;
+	    content["org.sblim.wbemsmt.ajax.AjaxPanelController.triggeredComponents"] = zoneId;
+	    
+	    //delete the first element
+	    this.threads.shift();
+	    //submit the request
+	    this.doAjaxSubmit(content, refreshTimeout, zoneId);
+	}
+	
+	//call the same method again
+	var ajaxCtrl = this;  
+	window.setTimeout(function() {ajaxCtrl.execute();}, 200)	
+}
 
 org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.togglePeriodicalUpdate = function(refreshTimeout, zoneId)
 {
@@ -70,7 +109,7 @@ org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.handleCallback = function(t
     if(type == "load")
     {
     
-    	addText("handleCallBack");
+    	console.log("handleCallBack");
     
 	    var componentUpdates = data.getElementsByTagName("component");
 	    var componentUpdate = null;
@@ -78,7 +117,7 @@ org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.handleCallback = function(t
 		for (var i = 0; i < componentUpdates.length; i++)
 		{
 			componentUpdate = componentUpdates[i];
-			addText("update component " + componentUpdate.getAttribute("id") + " HMTL " + componentUpdate.firstChild.data);
+			console.log("update component " + componentUpdate.getAttribute("id") + " HMTL " + componentUpdate.firstChild.data);
 			domElement = dojo.byId(componentUpdate.getAttribute("id"));
 			domElement.innerHTML = componentUpdate.firstChild.data;
 		}
@@ -114,11 +153,12 @@ org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.handleCallback = function(t
     {
         //alert("an Error occured during the ajax-request " + data.message);
     }
+    ajaxRequestIsRunning = false;
 }
 
 org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.doAjaxSubmit = function(content, refreshTimeout, zoneId)
 {   
-	addText("doAjaxSubmit");
+	console.log("doAjaxSubmit");
 
 	var ajaxCtrl = this;  
     var requestUri = "";
@@ -134,20 +174,26 @@ org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.doAjaxSubmit = function(con
 
     content["org.sblim.wbemsmt.ajax.AjaxPanelController.ajaxRequest"]="true";
 
-    dojo.io.bind({
-        url		: requestUri,
-        method	: "post",
-        useCache: false,
-        content	: content,
-        handle	: this.handleCallback,
-        mimetype: "text/xml",
-        transport: "XMLHTTPTransport",
-        formNode: this.form
-    });
+	if (ajaxRequestIsRunning == false)
+	{
+		
+		ajaxRequestIsRunning = true;
+	    
+	    dojo.io.bind({
+	        url		: requestUri,
+	        method	: "post",
+	        useCache: false,
+	        content	: content,
+	        handle	: this.handleCallback,
+	        mimetype: "text/xml",
+	        transport: "XMLHTTPTransport",
+	        formNode: this.form
+	    });
+	}
 
     if(refreshTimeout)
     {
-    	addText("New Timeout: " + zoneId + " timeout " + refreshTimeout);
+    	console.log("New Timeout: " + zoneId + " timeout " + refreshTimeout);
         this.windowTimeout[zoneId] = window.setTimeout(function() {
             ajaxCtrl.startPeriodicalUpdate(refreshTimeout, zoneId);
         }, refreshTimeout)
@@ -155,4 +201,3 @@ org.sblim.wbemsmt.ajax.AjaxPanelController.prototype.doAjaxSubmit = function(con
 
     return false;
 };
-
